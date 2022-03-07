e7836b7d0ac8ebe167719779e36aae1c9d191157
Piyush Gupta <piyushgupta2601@gmail.com>
2022-03-07 19:46:09 +0530

    quiz ace app

--- app/src/main/java/com/example/quiz_ace/SignUpActivity.kt
+++ app/src/main/java/com/example/quiz_ace/SignUpActivity.kt
@@ -2,10 +2,13 @@ package com.example.quiz_ace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        Log.i( "info","sign up running");
     }
 }
