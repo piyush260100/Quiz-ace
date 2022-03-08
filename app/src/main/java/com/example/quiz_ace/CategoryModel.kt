b276b8d39bda997059583d249aed9fa97d236a69
Piyush Gupta <piyushgupta2601@gmail.com>
2022-03-08 12:49:42 +0530

    quiz ace app

---
+++ app/src/main/java/com/example/quiz_ace/CategoryModel.kt
@@ -0,0 +1,11 @@
+package com.example.quiz_ace
+
+class CategoryModel {
+
+    private var categoryId: String="";
+    private var categoryName: String="";
+    private var categoryImage: String="";
+
+    public constructor() {}
+
+}
